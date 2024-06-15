314c57b20f17bb92f7433166da2b7d61bd99b1c1
AnkitKeshri <117108282+AnkitKeshri@users.noreply.github.com>
2024-06-15 23:20:07 +0530

    Update lecture14.py

--- lecture14.py
+++ lecture14.py
@@ -1,5 +1,21 @@
 #Functions is a block of reusable code
 
+def square(a):  #defining a function
+  print(a**2)
+
+a=int(input("Enter the value of a= ")) 
+square(a) #calling a function 
+#without calling nothing is printed
+
+
+def square(a):  #defining a function
+  return a**2
+
+a=int(input("Enter the value of a= ")) 
+c = square(a) #calling a function 
+print("c = " ,c)
+
+
 def add_numbers(a,b):
 """ Function to add two numbers"""
 return a + b
@@ -16,3 +32,29 @@ print(greet("Bob"))
 print(greet("Alice","Hi"))
 
 
+#Function with variable-Length Arguments
+def sum_all(*args):
+"""Function to sum all given arguments"""
+return sum(args)
+
+#Calling the function
+print(sum_all(1,2,3,4,5))
+
+
+#Function with Keyword Arguments
+def display_info(**kwargs):
+  """Function to display information using keyword arguments"""
+  for key, value in kwarsg.items():
+      print(f"{key}: {value}")
+
+#calling the function
+display_info(name="John", age=30, city="New York")
+
+
+#Highest-Order function
+def apply_function(func,x,y):
+  """Function to apply another function to give aarguments"""
+    return func(x,y)
+
+def multiply(a,b):
+  return a * b
