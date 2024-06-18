42189cd998caecdf58363700547cb500a975fd3b
AnkitKeshri <117108282+AnkitKeshri@users.noreply.github.com>
2024-06-19 00:31:01 +0530

    Matplotlib  Update lecture18.py

--- lecture18.py
+++ lecture18.py
@@ -25,20 +25,100 @@ x = [1,2,3,4,5]
 y = [2,3,5,7,11]
 
 #Creating a line plot
-plt.plot(x,y)
+plt.plot(x,y) #this will print simple solid line in the graph
+plt.plot(x,y,linestyle="dotted",color = '#5D6D7E',linewidth ="7",marker="o",mfc="r",ms="10") or plt.plot(x,y,ls=":") #this will print dotted line
+#Color will change the color of the line
+#linewidth or lw will change the line width of the line
+#in marker we can change the coordinated point to rectangle or circle and in different types of shaped also.
+#mfc will change the marker color
+#mec will change the marker boundary color 
+#ms use to change the marker size at 1 it will small and it goes bigger and bigger 
+plt.plot(x,y,linestyle="dashed") or plt.plot(x,y,ls="--") #this will print dashed line
 plt.xlabel('X-axis')
 plt.ylable('Y-axis')
 plt.title('Simple Line Plot')
 plt.show()
 
+#example fo 2 line graph
+import numpy as np
+y1=np.array([3,5,3,8])
+y2=np.array([1,9,7,4])
+plt.plot(y1)
+plt.plot(y2)
+plt.show
 
 
 
+#Creating a Simple Scatter Plot with MAtplotlib
+
+#Data
+x = [1,2,3,4,5]
+y = [2,3,5,7,11]
+
+#create a scatter plot
+plt.scatter(x,y,color="red") #this will only show the marker points (coordinates points)
+plt.xlabel('sales')
+plt.ylable('price')
+plt.title('Simple Scatter Plot')
+plt.show()
 
 
 
+#create a simple Bar Plot with Matplotlib
+# Data
+products = ['PC', 'TV', 'Ref', 'Micro']
+sales = [4, 7, 1, 8]
+
+# Creating a bar plot
+plt.bar(products, sales, color="black",width=0.1) #it will create verticle ber graph
+plt.barh(products, sales, color="black",width=0.1) #it will create horizontal ber graph. instead of bar I have written barh 
+plt.xlabel('products')
+plt.ylabel('Sales(in 1000s)')
+plt.title('Simple Bar Plot')
+plt.show()
+
+
+#Create a Simple Histogram with Matplotlib
+# Data
+data = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+
+#Creating a histogram
+plt.hist(data, bins=4) 
+plt.xlabel('Value']
+plt.ylabel('Frequency')
+plt.title('Simple Histogram')
+plt.show()
+
+
+#Creating Subplots with Matplotlib
+
+# Data
+x = [1, 2, 3, 4, 5]
+y1 = [2, 3, 5, 7, 11]
+y2=[1, 4, 6, 8, 10]
+
+# Creating subplots
+fig, axs = plt.subplots(2)
+axs[0].plot(x, y1)
+axs[0].set_title('First Plot')
+axs[1].plot(x, y2, 'tab:orang')
+axs[1].set_title('Second Plot')
+
+# Displaying the plot
+plt.tight_layout()
+plt.show()
 
 
 
+#Adding Annotations with Matplotlib
+# Data
+x = [1, 2, 3, 4, 5]
+y = [2, 3, 5, 7, 11]
 
-
+# Creating a plot with annotations
+plt.plot(x, y)
+plt.xlabel('X-axis')
+plt.ylabel('Y-axis')
+plt.title('Plot with Annotations')
+plt.annotate('Peak', xy=(5, 11), xytext=(4, 10), arrowprops=dict(facecolor='black', shrink=0.05)) #it will show peak written at 4,10 and a arrow pointing at 5,11
+plt.show()
